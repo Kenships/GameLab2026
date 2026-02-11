bce0a2c42ecc774396a2ecb511125ef8d3e1fbeb
Kenneth Xu <kenships0402@gmail.com>
2026-02-10 22:03:26 -0500

    NES Action added

---
+++ Assets/_Project/Scripts/Core/InputManagement/Interfaces/INESActionReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Project.Scripts.Core.InputManagement.Interfaces
+{
+    public class INESActionReader
+    {
+        public UnityEvent<Vector2> OnDPadInput;
+        public UnityEvent OnHoldInteract;
+        public UnityEvent OnReleaseInteract;
+        public UnityEvent OnDoubleTapInteract;
+        public UnityEvent OnHoldAltInteract;
+        public UnityEvent OnReleaseAltInteract;
+        public UnityEvent OnDoubleTapAltInteract;
+    }
+}
