66983db8e9a01851db4e9d26bd4bf768a8172d1d
Kenneth Xu <kenships0402@gmail.com>
2026-02-26 23:41:48 -0500

    Changed player interaction to use of range detector

--- Assets/_Project/Scripts/Core/Player/InteractionsWithObject.cs
+++ Assets/_Project/Scripts/Core/Player/InteractionsWithObject.cs
@@ -1,24 +1,33 @@
+using System.Collections.Generic;
 using _Project.Scripts.Core.Grid;
 using _Project.Scripts.Core.InputManagement.Interfaces;
 using _Project.Scripts.Core.Modules.Base_Class;
+using _Project.Scripts.Core.Modules.Interface;
 using Sisus.Init;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using ILogger = _Project.Scripts.Util.Logger.Interface.ILogger;
 
 namespace _Project.Scripts.Core.Player
 {
+    [RequireComponent(typeof(RangeDetector))]
     public class InteractionsWithObject : MonoBehaviour<INESActionReader,IGridService, ILogger>
     {
         [SerializeField] private Transform frontOfPlayer;
+        private RangeDetector _rangeDetector;
+        private List<ITimeControllable> _controllables = new();
         private GameObject _currentIHoldingObject;
         private INESActionReader _inputReader;
         private IGridService _gridService;
         private ILogger _logger;
+
+        private bool _isTimeControlling;
         
         protected override void Init(INESActionReader nesActionReader, IGridService gridService, ILogger logger)
         {
             _inputReader = nesActionReader;
             _gridService = gridService;
+            _rangeDetector = GetComponent<RangeDetector>();
         }
         
         private void OnEnable()
@@ -26,8 +35,10 @@ namespace _Project.Scripts.Core.Player
             _inputReader.OnDoubleTapAltInteract += PickUpOrPutDown;
             
             _inputReader.OnHoldInteract += FastForward;
+            _inputReader.OnReleaseInteract += CancelFastForward;
             
             _inputReader.OnHoldAltInteract += Rewind;
+            _inputReader.OnReleaseAltInteract += CancelRewind;
         }
 
         private void OnDisable()
@@ -46,7 +57,7 @@ namespace _Project.Scripts.Core.Player
             if (!_currentIHoldingObject)
             {
                 GameObject obj = _gridService.GetObjectOnGrid(frontOfPlayer.position);
-                if(obj == null) return;
+                if(!obj) return;
                 if (!obj.TryGetComponent(out IHoldable holdable)) return;
                 
                 holdable.PickUp();
@@ -78,13 +89,25 @@ namespace _Project.Scripts.Core.Player
             {
                 return;
             }
-            
-            GameObject objOnGrid = _gridService.GetObjectOnGrid(frontOfPlayer.position);
 
-            if (objOnGrid && objOnGrid.TryGetComponent(out Module module))
+            _isTimeControlling = true;
+            _controllables = _rangeDetector.GetObjectTypeInRangeNoAlloc(_controllables);
+
+            foreach (ITimeControllable controllable in _controllables)
             {
-                module.FastForward();
+                controllable?.FastForward();
             }
+        }
+
+        private void CancelFastForward()
+        {
+            foreach (ITimeControllable controllable in _controllables)
+            {
+                controllable?.CancelFastForward();
+            }
+            _controllables.Clear();
+            
+            _isTimeControlling = false;
         }
 
         // Hold B
@@ -95,18 +118,31 @@ namespace _Project.Scripts.Core.Player
             {
                 return;
             }
-            
-            GameObject objOnGrid = _gridService.GetObjectOnGrid(frontOfPlayer.position);
 
-            if (objOnGrid && objOnGrid.TryGetComponent(out Module module))
+            _isTimeControlling = true;
+            _controllables = _rangeDetector.GetObjectTypeInRangeNoAlloc(_controllables);
+
+            foreach (ITimeControllable controllable in _controllables)
             {
-                module.Rewind();
+                controllable?.Rewind();
             }
+        }
+
+        private void CancelRewind()
+        {
+            foreach (ITimeControllable controllable in _controllables)
+            {
+                controllable?.CancelRewind();
+            }
+
+            _controllables.Clear();
+            
+            _isTimeControlling = false;
         }
         
         private bool CanInteract()
         {
-            return !_currentIHoldingObject;
+            return !_currentIHoldingObject && !_isTimeControlling;
         }
     }
 }
