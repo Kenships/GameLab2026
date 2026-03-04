456768496acf918cf57522f73b6d0f389dbd9ac6
Miktak <mikitotakata@gmail.com>
2026-03-04 11:11:09 -0500

    created programmer art for car and empty script

---
+++ Assets/_Project/Scripts/Core/Modules/Car.cs
@@ -0,0 +1,55 @@
+using _Project.Scripts.Core.Modules.Base_Class;
+using UnityEngine;
+
+public class Car : Module
+{
+    protected override void LoadState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void AttackState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UsedState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnStateChanged(ModuleState newState)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Rewind()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void CancelRewind()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void CancelFastForward()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ShowVisual(int playerIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HideVisual(int playerIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FastForward()
+    {
+        throw new System.NotImplementedException();
+    }
+}
