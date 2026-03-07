6a71b795b4e84471860552bee865607247340f97
Miktak <mikitotakata@gmail.com>
2026-03-06 19:58:17 -0500

    car can move back and forth but not damage.

--- Assets/_Project/Scripts/Core/Modules/Car.cs
+++ Assets/_Project/Scripts/Core/Modules/Car.cs
@@ -1,55 +1,161 @@
+using System;
 using _Project.Scripts.Core.Modules.Base_Class;
+using _Project.Scripts.Core.Player;
+using System.Collections;
+using _Project.Scripts.Core.HealthManagement;
 using UnityEngine;
 
 public class Car : Module
 {
+
+    [SerializeField] private float fastForwardSpeed = 15f;  
+    [SerializeField] private float rewindSpeed = 10f;  
+    [SerializeField] private float rewindDistanceLimit = 5f;
+    [SerializeField] private float rewindDamage = 10f;
+    [SerializeField] private float fastFowardDamage = 20f;
+    
+    [SerializeField] private LayerMask _enemyLayer;
+    
+    private Vector3 _rewindLimitPosition;
+    private bool fullyRewound = false; 
+    private Vector3 _originalPosition;
+    private ModuleState _state = ModuleState.Used;
+    private Coroutine _rewindCoroutine;
+    private Coroutine _fastForwardCoroutine;
+    private bool fastForwarding = false;
+    private bool rewinding = false;
+    
+    void Start()
+    {
+        _originalPosition = transform.position;
+        _rewindLimitPosition = _originalPosition + new Vector3(0,0,rewindDistanceLimit);
+        
+    }
     protected override void LoadState()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     protected override void AttackState()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     protected override void UsedState()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     protected override void OnStateChanged(ModuleState newState)
     {
-        throw new System.NotImplementedException();
+        
+    }
+    
+    public override void FastForward()
+    {
+        if (_fastForwardCoroutine != null)
+        {
+            StopCoroutine(_fastForwardCoroutine);
+        }
+        fastForwarding = true;
+        _fastForwardCoroutine = StartCoroutine(FastForwardCoroutine());
+    }
+    
+    public override void CancelFastForward()
+    {
+        if (_fastForwardCoroutine != null)
+        {
+            StopCoroutine(_fastForwardCoroutine);
+            _fastForwardCoroutine = null;
+        }
+        fastForwarding = false;
+        
+    }
+    
+    private IEnumerator FastForwardCoroutine()
+    {
+        while (transform.position != _originalPosition)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                _originalPosition,
+                fastForwardSpeed * Time.deltaTime
+            );
+            
+            yield return null;
+        }
+        
+        _fastForwardCoroutine = null;
     }
 
     public override void Rewind()
     {
-        throw new System.NotImplementedException();
+        if (_rewindCoroutine != null)
+        {
+            StopCoroutine(_rewindCoroutine);
+        }
+
+        rewinding = true;
+        _rewindCoroutine = StartCoroutine(RewindCoroutine());
     }
 
     public override void CancelRewind()
     {
-        throw new System.NotImplementedException();
+        if (_rewindCoroutine != null)
+        {
+            StopCoroutine(_rewindCoroutine);
+            _rewindCoroutine = null;
+        }
+        rewinding =  false;
+    }
+    
+    private IEnumerator RewindCoroutine()
+    {
+        while (transform.position != _rewindLimitPosition)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                _rewindLimitPosition,
+                rewindSpeed * Time.deltaTime
+            );
+            
+            yield return null;
+        }
+        
+        _rewindCoroutine = null;
     }
 
-    public override void CancelFastForward()
+    private void OnTriggerEnter(Collider other)
     {
-        throw new System.NotImplementedException();
+        if ((_enemyLayer.value & (1 << other.gameObject.layer)) != 0)
+        {
+            Debug.Log(other.gameObject.name);
+            if (other.TryGetComponent(out IDamageable damageable))
+            {
+                if (fastForwarding == true)
+                {
+                    damageable.Damage(fastFowardDamage);
+                }
+
+                if (rewinding == true)
+                {
+                    damageable.Damage(rewindDamage);
+                }
+
+            }
+        }
     }
 
-    public override void ShowVisual(int playerIndex)
+    public override void ShowVisual(PlayerData.PlayerID playerID)
     {
-        throw new System.NotImplementedException();
+        
     }
 
-    public override void HideVisual(int playerIndex)
+    public override void HideVisual(PlayerData.PlayerID playerID)
     {
-        throw new System.NotImplementedException();
+      
     }
+    
 
-    public override void FastForward()
-    {
-        throw new System.NotImplementedException();
-    }
+    
 }
