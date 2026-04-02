e617e392eb1d19806cc127e85e4d7b3dc27240e3
Kenneth <kenships0402@gmail.com>
2026-04-02 08:56:08 -0400

    Added Light Indicators for car and VHS

--- Assets/_Project/Scripts/Tutorial/TutCarListener.cs
+++ Assets/_Project/Scripts/Tutorial/TutCarListener.cs
@@ -1,4 +1,5 @@
 using System;
+using PrimeTween;
 using UnityEngine;
 
 namespace _Project.Scripts.Tutorial
@@ -6,20 +7,42 @@ namespace _Project.Scripts.Tutorial
     public class TutCarListener : MonoBehaviour, ITutorialListener
     {
         [SerializeField] private TutCar tutCar;
+        [SerializeField] private Light carLight;
 
         private Action _callback;
+        private float _initialLightIntensity;
         
         public void Invoke(Action callback)
         {
             _callback = callback;
-            
+            _initialLightIntensity = carLight.intensity;
+            PulseCarLight();
             tutCar.OnCarAttack += OnCarAttack;
+        }
+
+        private void OnDestroy()
+        {
+            tutCar.OnCarAttack -= OnCarAttack;
+            Tween.StopAll(carLight);
+        }
+
+        private void PulseCarLight()
+        {
+            Tween.LightIntensity(
+                target: carLight,
+                startValue: _initialLightIntensity,
+                endValue: 10f,
+                duration: 1f,
+                ease: Ease.InOutQuad,
+                cycleMode: CycleMode.Yoyo)
+                .SetRemainingCycles(-1);
         }
 
         private void OnCarAttack()
         {
             tutCar.OnCarAttack -= OnCarAttack;
-            
+            Tween.StopAll(carLight);
+            carLight.intensity = _initialLightIntensity;
             _callback?.Invoke();
         }
     }
--- Assets/_Project/Scripts/Tutorial/TutVHSListener.cs
+++ Assets/_Project/Scripts/Tutorial/TutVHSListener.cs
@@ -3,6 +3,7 @@ using _Project.Scripts.Core;
 using _Project.Scripts.Core.HealthManagement;
 using _Project.Scripts.Core.Modules;
 using Obvious.Soap;
+using PrimeTween;
 using UnityEngine;
 
 namespace _Project.Scripts.Tutorial
@@ -10,10 +11,13 @@ namespace _Project.Scripts.Tutorial
     public class TutVHSListener : MonoBehaviour, ITutorialListener
     {
         [SerializeField] private VHSModule vhsModule;
+        [SerializeField] private Light vhsLight;
         
         private Action _callback;
         
         private Health _health;
+        
+        private float _initialLightIntensity;
         
         private void Awake()
         {
@@ -23,18 +27,35 @@ namespace _Project.Scripts.Tutorial
         private void OnDestroy()
         {
             _health.OnFullHp -= VhsHPFullOnRaised;
+            Tween.StopAll(vhsLight);
         }
 
         public void Invoke(Action callback)
         {
             _callback = callback;
+            _initialLightIntensity = vhsLight.intensity;
+
+            PulseLight();
             
-            vhsModule.EnableModule = true;
             _health.OnFullHp += VhsHPFullOnRaised;
+        }
+
+        private void PulseLight()
+        {
+            Tween.LightIntensity(
+                target: vhsLight,
+                startValue: _initialLightIntensity,
+                endValue: 15f,
+                duration: 1f,
+                ease: Ease.InOutQuad,
+                cycleMode: CycleMode.Yoyo)
+                .SetRemainingCycles(-1);
         }
 
         private void VhsHPFullOnRaised()
         {
+            Tween.StopAll(vhsLight);
+            vhsLight.intensity = _initialLightIntensity;
             _health.OnFullHp -= VhsHPFullOnRaised;
             _callback?.Invoke();
         }
