c9c2abe9e91a9ff168243c7975888c614108bb57
Kenneth <kenships0402@gmail.com>
2026-04-04 17:32:52 -0400

    fully implemented coop tutorial listener

--- Assets/_Project/Scripts/Tutorial/TutCoopInteractListener.cs
+++ Assets/_Project/Scripts/Tutorial/TutCoopInteractListener.cs
@@ -1,4 +1,5 @@
 using System;
+using _Project.Scripts.Core.Modules.Base_Class;
 using _Project.Scripts.Core.Player;
 using UnityEngine;
 
@@ -8,14 +9,12 @@ namespace _Project.Scripts.Tutorial
     {
         [SerializeField] private TutPlayerInteractionController player1;
         [SerializeField] private TutPlayerInteractionController player2;
-
-        private bool _startCheck;
+        
         private Action _callback;
         
         public void Invoke(Action callback)
         {
             _callback = callback;
-            _startCheck = true;
             
             player1.OnRewind += TimeControl;
             player2.OnRewind += TimeControl;
@@ -25,7 +24,16 @@ namespace _Project.Scripts.Tutorial
 
         private void TimeControl(PlayerData.PlayerID playerID)
         {
-            
+            if (player1.HeldModule && player1.HeldModule.GetComponent<Module>().IsTimeControlling
+                || (player2.HeldModule && player2.HeldModule.GetComponent<Module>().IsTimeControlling))
+            {
+                player1.OnRewind -= TimeControl;
+                player2.OnRewind -= TimeControl;
+                player1.OnFastForward -= TimeControl;
+                player2.OnFastForward -= TimeControl;
+                
+                _callback?.Invoke();
+            }
         }
     }
 }
