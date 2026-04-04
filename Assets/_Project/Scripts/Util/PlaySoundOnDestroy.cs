0313287cb1725b1e141e57b4f66b374dde3a8d6b
MMBBB139 <143266105+MMBBB139@users.noreply.github.com>
2026-04-04 18:49:00 -0400

    Provided a slot to drag in enemy death sound

---
+++ Assets/_Project/Scripts/Util/PlaySoundOnDestroy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using AudioType = _Project.Scripts.Core.AudioPooling.Interface.AudioType;
+using Sisus.Init;
+
+using _Project.Scripts.Core.AudioPooling;
+
+public class PlaySoundOnDestroy : MonoBehaviour<AudioPooler>
+{
+    [SerializeField] private AudioClip sound;
+    [SerializeField] private float volume;
+    private AudioPooler _pooler;
+    protected override void Init(AudioPooler argument)
+    {
+        _pooler = argument;
+    }
+
+    private void OnDestroy()
+    {
+        _pooler.New2DAudio(sound).OnChannel(AudioType.Sfx).SetVolume(volume).Play();
+    }
+}
