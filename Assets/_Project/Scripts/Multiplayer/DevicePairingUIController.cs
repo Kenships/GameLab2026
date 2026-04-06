dd3a2fc16cadb9d30871bacd541e55d769d69b87
Kenneth <kenships0402@gmail.com>
2026-04-06 15:43:37 -0400

    Player select complete

--- Assets/_Project/Scripts/Multiplayer/DevicePairingUIController.cs
+++ Assets/_Project/Scripts/Multiplayer/DevicePairingUIController.cs
@@ -1,8 +1,10 @@
 using _Project.Scripts.Core.Player;
 using _Project.Scripts.Core.SceneLoading;
 using _Project.Scripts.UI;
+using PrimeTween;
 using Sisus.Init;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.Multiplayer
 {
@@ -16,11 +18,17 @@ namespace _Project.Scripts.Multiplayer
 
         [SerializeField] private GameObject player1SelectVisual;
         [SerializeField] private GameObject player2SelectVisual;
+        
+        [SerializeField] private GameObject player1GhostVisual;
+        [SerializeField] private GameObject player2GhostVisual;
 
-        [SerializeField] private GameObject player1ConfirmVisual;
-        [SerializeField] private GameObject player2ConfirmVisual;
+        [SerializeField] private GameObject redConfirmVisual;
+        [SerializeField] private GameObject blueConfirmVisual;
 
         [SerializeField] private Transform[] treys;
+
+        private bool _player1Init;
+        private bool _player2Init;
         
         private bool _player1Ready;
         private bool _player2Ready;
@@ -30,6 +38,8 @@ namespace _Project.Scripts.Multiplayer
         
         private IDevicePairingService _devicePairingService;
         private SceneLoader _sceneLoader;
+
+        private bool _bufferFrame;
         
         protected override void Init(IDevicePairingService devicePairingService)
         {
@@ -39,9 +49,6 @@ namespace _Project.Scripts.Multiplayer
 
         private void OnEnable()
         {
-            _player1Index = 1;
-            _player2Index = 1;
-            
             player1.OnMove += PlayerOnMove;
             player2.OnMove += PlayerOnMove;
             
@@ -55,6 +62,29 @@ namespace _Project.Scripts.Multiplayer
             _devicePairingService.OnPlayer2Paired += DevicePairingServiceOnPlayer2Paired;
             _devicePairingService.OnPlayer1Unpaired += DevicePairingServiceOnPlayer1Unpaired;
             _devicePairingService.OnPlayer2Unpaired += DevicePairingServiceOnPlayer2Unpaired;
+        }
+        
+        private void OnDisable()
+        {
+            if (_devicePairingService == null)
+            {
+                Debug.LogWarning("DevicePairingService is null");
+                return;
+            }
+            
+            player1.OnMove -= PlayerOnMove;
+            player2.OnMove -= PlayerOnMove;
+            
+            player1.OnConfirm -= PlayerOnConfirm;
+            player2.OnConfirm -= PlayerOnConfirm;
+            
+            player1.OnCancel -= PlayerOnCancel;
+            player2.OnCancel -= PlayerOnCancel;
+            
+            _devicePairingService.OnPlayer1Paired -= DevicePairingServiceOnPlayer1Paired;
+            _devicePairingService.OnPlayer2Paired -= DevicePairingServiceOnPlayer2Paired;
+            _devicePairingService.OnPlayer1Unpaired -= DevicePairingServiceOnPlayer1Unpaired;
+            _devicePairingService.OnPlayer2Unpaired -= DevicePairingServiceOnPlayer2Unpaired;
         }
 
         private void PlayerOnCancel(PlayerData.PlayerID playerID)
@@ -76,13 +106,17 @@ namespace _Project.Scripts.Multiplayer
             switch (playerID)
             {
                 case PlayerData.PlayerID.Player1:
-                    if (_player1Index != 1 && _player1Index != _player2Index){
+                    if (!_player1Init)
+                        return;
+                    if (_player1Index != _player2Index){
                         PlayerConfirm(_player1Index);
                         _player1Ready = true;
                     }
                     break;
                 case PlayerData.PlayerID.Player2:
-                    if (_player2Index != 1 && _player2Index != _player1Index)
+                    if (!_player2Init)
+                        return;
+                    if (_player2Index != _player1Index)
                     {
                         PlayerConfirm(_player2Index);
                         _player2Ready = true;
@@ -112,6 +146,9 @@ namespace _Project.Scripts.Multiplayer
 
         private void PlayerOnMove((PlayerData.PlayerID id, int dir) arg)
         {
+            if (arg.dir == 0)
+                return;
+            
             switch (arg.id)
             {
                 case PlayerData.PlayerID.Player1:
@@ -119,6 +156,7 @@ namespace _Project.Scripts.Multiplayer
                     {
                         return;
                     }
+                    _player1Init = true;
                     UpdateSelectIndex(ref _player1Index, arg.dir);
                     break;
                 case PlayerData.PlayerID.Player2:
@@ -126,30 +164,17 @@ namespace _Project.Scripts.Multiplayer
                     {
                         return;
                     }
+                    _player2Init = true;
                     UpdateSelectIndex(ref _player2Index, arg.dir);
                     break;
             }
             UpdateUI();
         }
 
-        private void OnDisable()
-        {
-            if (_devicePairingService == null)
-            {
-                Debug.LogWarning("DevicePairingService is null");
-                return;
-            }
-            
-            _devicePairingService.OnPlayer1Paired -= DevicePairingServiceOnPlayer1Paired;
-            _devicePairingService.OnPlayer2Paired -= DevicePairingServiceOnPlayer2Paired;
-            _devicePairingService.OnPlayer1Unpaired -= DevicePairingServiceOnPlayer1Unpaired;
-            _devicePairingService.OnPlayer2Unpaired -= DevicePairingServiceOnPlayer2Unpaired;
-        }
+        
 
         private void Update()
         {
-            UpdateUI();
-
             if (_player1Ready && _player2Ready)
             {
                 _devicePairingService.SwapPlayers = _player1Index > _player2Index;
@@ -180,14 +205,57 @@ namespace _Project.Scripts.Multiplayer
 
         private void UpdateUI()
         {
-            player1SelectVisual.SetActive(_devicePairingService.IsPlayer1Paired);
-            player2SelectVisual.SetActive(_devicePairingService.IsPlayer2Paired);
+            player1SelectVisual.SetActive(_player1Init && _devicePairingService.IsPlayer1Paired);
+            player1GhostVisual.SetActive(_player1Init && _devicePairingService.IsPlayer1Paired);
+            player2SelectVisual.SetActive(_player2Init && _devicePairingService.IsPlayer2Paired);
+            player2GhostVisual.SetActive(_player2Init && _devicePairingService.IsPlayer2Paired);
             
-            player1ConfirmVisual.SetActive(_player1Ready);
-            player2ConfirmVisual.SetActive(_player2Ready);
+            blueConfirmVisual.SetActive((_player1Ready && _devicePairingService.IsPlayer1Paired && _player1Index == 0)
+                || (_player2Ready && _devicePairingService.IsPlayer2Paired && _player2Index == 0));
+            redConfirmVisual.SetActive((_player1Ready && _devicePairingService.IsPlayer1Paired && _player1Index == treys.Length - 1)
+                || (_player2Ready && _devicePairingService.IsPlayer2Paired && _player2Index == treys.Length - 1));
+
+            player1GhostVisual.transform.SetParent(treys[_player1Index]);
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) treys[0]);
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) treys[1]);
+            player1GhostVisual.transform.localPosition = 
+                new Vector3(player1GhostVisual.transform.localPosition.x, player1GhostVisual.transform.localPosition.y, 0);
             
-            player1SelectVisual.transform.SetParent(treys[_player1Index]);
-            player2SelectVisual.transform.SetParent(treys[_player2Index]);
+            
+            player2GhostVisual.transform.SetParent(treys[_player2Index]);
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) treys[0]);
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) treys[1]);
+            player2GhostVisual.transform.localPosition = 
+                new Vector3(player2GhostVisual.transform.localPosition.x, player2GhostVisual.transform.localPosition.y, 0);
+
+            
+            Tween.Position(
+                    target: player1SelectVisual.transform,
+                    endValue: player1GhostVisual.transform.position,
+                    duration: 0.2f,
+                    ease: Ease.Linear
+                ).Group(
+                    Tween.Rotation(
+                        target: player1SelectVisual.transform,
+                        endValue: player1GhostVisual.transform.rotation,
+                        duration: 0.2f,
+                        ease: Ease.Linear
+                    )
+                );
+
+            Tween.Position(
+                    target: player2SelectVisual.transform,
+                    endValue: player2GhostVisual.transform.position,
+                    duration: 0.2f,
+                    ease: Ease.Linear
+                ).Group(
+                    Tween.Rotation(
+                        target: player2SelectVisual.transform,
+                        endValue: player2GhostVisual.transform.rotation,
+                        duration: 0.2f,
+                        ease: Ease.Linear
+                    )
+                );
         }
     }
 }
