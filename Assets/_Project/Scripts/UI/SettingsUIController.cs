81d4e51c585b45bec713d5d75d265c2f15bf5b7e
Kenneth Xu <kenships0402@gmail.com>
2026-04-07 15:13:55 -0400

    Translations Finished outside of modules

--- Assets/_Project/Scripts/UI/SettingsUIController.cs
+++ Assets/_Project/Scripts/UI/SettingsUIController.cs
@@ -1,13 +1,37 @@
+using System;
 using Knot.Localization;
 using Obvious.Soap;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.UI
 {
     public class SettingsUIController : MonoBehaviour
     {
+        [SerializeField] Button[] languageButtons;
+        [SerializeField] Button[] hapticsButtons;
+        
         [SerializeField] private IntVariable language;
         [SerializeField] private FloatVariable hapticStrength;
+
+        private void Start()
+        {
+            languageButtons[language.Value].onClick.Invoke();
+            languageButtons[language.Value].Select();
+            
+            switch (hapticStrength.Value)
+            {
+                case > 0.5f:
+                    hapticsButtons[2].onClick.Invoke();
+                    break;
+                case > 0f:
+                    hapticsButtons[1].onClick.Invoke();
+                    break;
+                default:
+                    hapticsButtons[0].onClick.Invoke();
+                    break;
+            }
+        }
 
         public void SetLanguage(int languageIndex)
         {
